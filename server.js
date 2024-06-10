b829c3821a0f34d5e02e26e752fad315e4aff94e
AnthonyAmaker <tamaker@gmail.com>
2024-06-10 18:23:28 -0400

    initial and working commit

---
+++ server.js
@@ -0,0 +1,6 @@
+var arrCheckStreetNames = ['beechfern', 'giant feather loop', 'cordgrass', 'brushfield'];
+
+var testString = "beechfway"
+var checkForFoundStreetName = arrCheckStreetNames.some( substring => testString.toLowerCase().includes(substring) );
+
+console.log(checkForFoundStreetName)
